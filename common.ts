280a4f13523c82c8cd45e34176b820b1ae481530
eliot-akira <me@eliotakira.com>
2025-11-09 11:32:21 +0100

    Improving logic to get branch name in tag event context

--- common.ts
+++ common.ts
@@ -64,6 +64,7 @@ export async function getBranchNameFromTag(tag: string): Promise<string> {
         )
       ).stdout || ''
     )
+      .replace(/^origin\//, '')
       .replace(/^\* /, '')
       .trim()
     if (result) {
